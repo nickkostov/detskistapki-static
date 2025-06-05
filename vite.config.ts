3de7938f431c6d7d29ed1ac2e5b00d72a76fdffc
Nikolay Kostov <nikolay.kostov@weareplanet.com>
2025-06-05 23:14:47 +0300

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/detskistapki-static/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
